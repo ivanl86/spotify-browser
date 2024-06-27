ed4edc263b5e8e926738e2f370824d90998df144
Ivan Leung <ivanl8866@gmail.com>
2024-06-27 14:26:54 -0700

    Create a new-releases route.
    This route retrieves the newest releases from Spotify
    using the Spotify API.

---
+++ server/src/routes/browse/new-releases.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+import axios, { AxiosResponse } from 'axios';
+import TokenAuth from '../../services/token-auth';
+import Token from '../../model/token';
+
+const router = Router();
+const tokenAuth = new TokenAuth();
+
+router
+  .route("/")
+  .get(async (req, res) => {
+    try {
+      console.log("Getting new releases!");
+      const token: Token = await tokenAuth.getJson();
+      const response: AxiosResponse = await axios.get(
+        `${process.env.API_URL}/browse/new-releases?offset=${req.query.offset}&limit=${req.query.limit}`,
+        {
+          headers: {
+            Authorization: `${token.token_type} ${token.access_token}`
+          }
+        }
+      );
+      res.json(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+export default router;
