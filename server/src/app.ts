9ba6d80794bc9528db0a58c97e8812183b3a6da2
Ivan Leung <ivanl8866@gmail.com>
2024-06-27 14:22:45 -0700

    Enable cors to all routes.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import router from './routes/routes';
 import Logger from './middleware/logger';
+import cors from 'cors'
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(Logger.logRequest);
 app.use(Logger.logError);
