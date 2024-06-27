6b61df07b95f6e9c1923499468712140f15b981e
Ivan Leung <ivanl8866@gmail.com>
2024-06-27 14:31:02 -0700

    Attach browse route to the main route.
    The main route is "http://localhost:port/routes/".

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import albumRoute from './albums/albums';
 import artistRoute from './artists/artists';
 import trackRoute from './tracks/tracks';
+import browseRoute from './browse/browse'
 
 const router = Router();
 
 router.use("/albums", albumRoute);
 router.use("/artists", artistRoute);
+router.use("/browse", browseRoute);
 router.use("/tracks", trackRoute);
 
 export default router;
