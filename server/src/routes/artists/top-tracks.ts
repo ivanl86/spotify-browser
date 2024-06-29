21935fff3379221239a3371a3d4ee93a991458b6
Ivan Leung <ivanl8866@gmail.com>
2024-06-28 18:58:00 -0700

    Add a API endpoint to retrieve popular tracks.
    This API endpoint call a GET request to retrieve popular
    tracks of a artist. A artist ID is required to make tihs
    request.

---
+++ server/src/routes/artists/top-tracks.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+import axios, { AxiosResponse } from 'axios';
+import TokenAuth from '../../services/token-auth';
+import Token from '../../model/token';
+
+const router = Router();
+const tokenAuth = new TokenAuth();
+
+router
+  .route("/")
+  .get(async (req, res) => {
+    try {
+      const token: Token = await tokenAuth.getJson();
+      const response: AxiosResponse = await axios.get(
+        `${process.env.API_URL}/artists/${req.query.id}/top-tracks`,
+        {
+          headers: {
+            Authorization: `${token.token_type} ${token.access_token}`
+          }
+        }
+      );
+      res.json(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  export default router;
