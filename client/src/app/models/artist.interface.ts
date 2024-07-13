5d375cefed45753e8ec637c4bc2c45ed9db2117a
Ivan Leung <ivanl8866@gmail.com>
2024-07-12 22:19:45 -0700

    Add property 'followers'.

--- client/src/app/models/artist.interface.ts
+++ client/src/app/models/artist.interface.ts
@@ -2,6 +2,7 @@ import { Image } from './image.interface';
 
 export interface Artist {
   external_urls: { spotify: string };
+  followers: { total: number };
   id: string;
   name: string;
   type: string;
