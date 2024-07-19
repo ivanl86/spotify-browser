5c2d0e4369a18a114439fc17eec3cf2770c38220
Ivan Leung <ivanl8866@gmail.com>
2024-07-18 22:40:39 -0700

    Allow reload when there is a new query entered.

--- client/src/app/search/search-result/search-result.component.ts
+++ client/src/app/search/search-result/search-result.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Observable, Subscription, map } from 'rxjs';
 import { SpotifyService } from '../../services/spotify.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, NavigationEnd } from '@angular/router';
 import { Album } from '../../models/album.interface';
 import { Artist } from '../../models/artist.interface';
 import { Track } from '../../models/track.interface';
-import { Image } from '../../models/image.interface';
 import { AlbumList } from '../../models/album-list.interface';
 import { ArtistList } from '../../models/artist-list.interface';
 import { TrackList } from '../../models/track-list.interface';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-result',
@@ -17,54 +18,69 @@ import { TrackList } from '../../models/track-list.interface';
 })
 export class SearchResultComponent implements OnDestroy {
 
-  private routeSubscription: Subscription;
-  private albumSubscription: Subscription;
-  private artistSubscription: Subscription;
-  private trackSubscription: Subscription;
+  private routerSubscription: Subscription = <Subscription>{};
+  private activatedRouteSubscription: Subscription = <Subscription>{};
+  private albumSubscription: Subscription = <Subscription>{};
+  private artistSubscription: Subscription = <Subscription>{};
+  private trackSubscription: Subscription = <Subscription>{};
   private query: string = "";
 
   albumList: AlbumList = <AlbumList>{};
   artistList: ArtistList = <ArtistList>{};
   trackList: TrackList = <TrackList>{};
+  tracksMidIndex: number = 0;
 
   constructor(
+    private router: Router,
     private activatedRoute: ActivatedRoute,
-    private spotifyService: SpotifyService
+    private spotifyService: SpotifyService,
+    private titleService: Title
   ) {
-    this.routeSubscription = this.activatedRoute.queryParamMap.subscribe(
-      params => {
-        const query: string | null = params.get("query");
-        if (query) {
-          this.query = query;
+
+    this.routerSubscription = this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        if (this.router.url.includes("/search")) {
+
+          this.activatedRouteSubscription = this.activatedRoute.queryParamMap.subscribe(
+            params => {
+              const query: string | null = params.get("query");
+              if (query) {
+                this.query = query;
+                this.titleService.setTitle(`Search ${query}`);
+              }
+            }
+          );
+
+          this.albumSubscription = this.search(this.query, "album").subscribe(
+            response => {
+              this.albumList = response as AlbumList;
+            }
+          );
+          this.artistSubscription = this.search(this.query, "artist").subscribe(
+            response => {
+              this.artistList = response as ArtistList;
+            }
+          );
+          this.trackSubscription = this.search(this.query, "track").subscribe(
+            response => {
+              this.trackList = response as TrackList;
+              this.tracksMidIndex = Math.ceil(this.trackList.tracks.items.length / 2);
+            }
+          );
+
         }
       }
-    );
-
-    this.albumSubscription = this.search(this.query, "album").subscribe(
-      response => {
-        this.albumList = response as AlbumList;
-      }
-    );
-    this.artistSubscription = this.search(this.query, "artist").subscribe(
-      response => {
-        this.artistList = response as ArtistList;
-      }
-    );
-    this.trackSubscription = this.search(this.query, "track").subscribe(
-      response => {
-        this.trackList = response as TrackList;
-      }
-    );
+    });
   }
 
   private search(query: string, type: string, limit: number = 20, offset: number = 0): Observable<AlbumList | ArtistList | TrackList> {
     return this.spotifyService
-    .search(query, type, limit, offset)
-    .pipe(
-      map(response => {
-        return response;
-      })
-    )
+      .search(query, type, limit, offset)
+      .pipe(
+        map(response => {
+          return response;
+        })
+      )
   }
 
   get albums(): Album[] {
@@ -83,7 +99,8 @@ export class SearchResultComponent implements OnDestroy {
     this.trackSubscription.unsubscribe();
     this.artistSubscription.unsubscribe();
     this.albumSubscription.unsubscribe();
-    this.routeSubscription.unsubscribe();
+    this.activatedRouteSubscription.unsubscribe();
+    this.routerSubscription.unsubscribe();
   }
 
 }
