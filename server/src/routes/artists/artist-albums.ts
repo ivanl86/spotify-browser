7d34551ea5aaa75644fefc994c87cf1dbfcc9b0c
Ivan Leung <ivanl8866@gmail.com>
2024-07-22 15:20:35 -0700

    Create API endpoint artist-albums.
    This GET request retrieves the albums of an artist.
    An artist's id is required.

---
+++ server/src/routes/artists/artist-albums.ts
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+import axios, { AxiosResponse } from 'axios';
+import TokenAuth from '../../services/token-auth';
+import Token from '../../model/token';
+
+const router = Router();
+const tokenAuth = new TokenAuth();
+
+router
+  .route("/")
+  .get(async (req, res) => {
+    try {
+      const token: Token = await tokenAuth.getJson();
+      const response: AxiosResponse = await axios.get(
+        `${process.env.API_URL}/artists/${req.query.id}/albums`,
+        {
+          headers: {
+            Authorization: `${token.token_type} ${token.access_token}`
+          },
+          params: {
+            limit: req.query.limit,
+            offset: req.query.offset
+          }
+        }
+      );
+      res.json(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  export default router;
