0a4a2d1d4e22df957d3a996f73c64d6977871dcd
Ivan Leung <ivanl8866@gmail.com>
2024-07-24 14:22:18 -0700

    Refactor routeSubscription to subscribe multiple services.

--- client/src/app/artists/artist/artist.component.ts
+++ client/src/app/artists/artist/artist.component.ts
@@ -2,10 +2,11 @@ import { Component, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SpotifyService } from '../../services/spotify.service';
 import { Artist } from '../../models/artist.interface';
-import { Subscription } from 'rxjs';
+import { Subscription, switchMap } from 'rxjs';
 import { Track } from '../../models/track.interface';
 import { Image } from '../../models/image.interface';
 import { Title } from '@angular/platform-browser';
+import { Album } from '../../models/album.interface';
 
 @Component({
   selector: 'app-artist',
@@ -15,37 +16,45 @@ import { Title } from '@angular/platform-browser';
 export class ArtistComponent implements OnDestroy {
 
   private routeSubscription: Subscription;
-  private spotifySubscription: Subscription;
-  private topTracksSubscription: Subscription;
   private id: string = "";
 
   artist: Artist = <Artist>{ };
   tracks: Track[] = [];
+  albums: Album[] = [];
+  relatedArtists: Artist[] = [];
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private spotifyService: SpotifyService,
     private titleService: Title
   ) {
-    this.routeSubscription = this.activatedRoute.queryParamMap.subscribe(
-      params => {
-        const id: string | null = params.get("id");
-        if (id) {
-          this.id = id;
+    this.routeSubscription = this.activatedRoute.queryParamMap
+      .pipe(
+        switchMap(params => {
+          this.id = params.get("id") ?? "";
+          return this.spotifyService.getArtist(this.id);
+        }),
+        switchMap(artist => {
+          this.artist = artist;
+          this.titleService.setTitle(this.name);
+          return this.spotifyService.getTopTracks(this.id);
+        }),
+        switchMap(trackList => {
+          this.tracks = trackList.tracks;
+          return this.spotifyService.getArtistAlbums(this.id);
+        }),
+        switchMap(albums => {
+          this.albums = albums.items;
+          return this.spotifyService.getRelatedArtists(this.id);
+        })
+      )
+      .subscribe({
+        next: relatedArtists => {
+          this.relatedArtists = relatedArtists.artists;
+        },
+        error: error => {
+          console.log(error);
         }
-      });
-
-    this.spotifySubscription = this.spotifyService
-      .getArtist(this.id)
-      .subscribe(response => {
-        this.artist = response;
-        this.titleService.setTitle(this.name);
-      });
-
-    this.topTracksSubscription = this.spotifyService
-      .getTopTracks(this.id)
-      .subscribe(response => {
-        this.tracks = response.tracks;
       });
   }
 
@@ -74,8 +83,6 @@ export class ArtistComponent implements OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.topTracksSubscription.unsubscribe();
-    this.spotifySubscription.unsubscribe();
     this.routeSubscription.unsubscribe();
   }
 }
